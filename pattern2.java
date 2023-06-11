ce80ae7cf1b815a4616deaea766d4848fc5cb399
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2023-06-11 11:18:02 +0530

    patterns problem done

--- pattern2.java
+++ pattern2.java
@@ -49,5 +49,51 @@ public class pattern2 {
             }
             System.out.println();
         }
+        // number triangle 
+        for(int i=1; i<=n; i++) {
+            //spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            //numbers ->print row no, row no times
+            for(int j=1; j<=i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        //palindromic 
+        for ( int i = 1 ; i <= n ; i++){
+            for ( int j = 1 ; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for ( int j = i ; j>=1; j--){
+                System.out.print(j);
+            }
+            for ( int j = 2 ; j <= i ; j++){
+                System.out.print(j);
+            }
+            System.out.println(); 
+        }
+        //diamond
+        // upper half 
+        for( int i = 1 ; i <=n ; i ++){
+            for ( int j = 1 ; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for ( int j = 1 ; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lowerhalf
+        for( int i = n ; i >=1 ; i--){
+            for ( int j = 1 ; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for ( int j = 1 ; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
