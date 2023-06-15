d7057ba0814f42e70bebffc58e182b31e2ad4036
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2023-06-15 09:59:05 +0530

    some changes in functions.java

--- functions.java
+++ functions.java
@@ -5,19 +5,11 @@ public class functions{
             System.out.println("Hello " +name);
             return;
     }
-    public static int sumOfTwoNumbers(int a , int b){
-        return a+b;
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Your name is :- ");
         String name = sc.next();
         printMyName(name);
-        System.out.print("print first number :- ");
-        int a = sc.nextInt();
-        System.out.print("print second number :- ");
-        int b = sc.nextInt();
-        System.out.print("THe sum of two no. are "+sumOfTwoNumbers(a, b));
         sc.close();
     }
 
